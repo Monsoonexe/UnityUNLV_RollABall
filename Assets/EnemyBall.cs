c9d55d1136117cd64456d62a11282bdc6b3919c3
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2018-09-28 19:57:32 -0700

    Rich's Unity files from Friday Workshop
    This is Rich's version.  It may be slightly different than yours.

---
+++ Assets/EnemyBall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBall : MonoBehaviour {
+    private Collider col;
+    public int bumpForce = 1;
+
+	// Use this for initialization
+	void Start () {
+        col = gameObject.GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            //Vector3 bumpVector = new Vector3(collision.);
+            playerRigidbody.AddForce( transform.position * bumpForce);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rigidBody;
+    private Collider col;
+    public Text scoreText;
+    public int speed;
+
+    public int totalScore = 0;
+    public int pointValue = 5;
+
+    private void Start()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+        col = gameObject.GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        scoreText.text = "Score: " + totalScore.ToString();
+		
+	}
+
+    //called before physics step
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rigidBody.AddForce(movement * speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Pickup"))
+        {
+            totalScore += pointValue;
+            Destroy(collision.gameObject);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
