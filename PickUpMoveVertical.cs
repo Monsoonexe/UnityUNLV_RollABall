e90fe57983cfeaaa8aba993306bcee151e31cb4c
NightlyShadow <43693128+NightlyShadow@users.noreply.github.com>
2018-10-05 10:21:45 -0700

    Added more collectible types
    A little housekeeping and added two more collectibles
    <details>
    Did some housekeeping- pickup component had the pickup rotator script 3 times. Deleted 2 of the scripts to get rid of the redundancy
    Added a folder for Collectibles under Prefabs
    Renamed default pickup to “PickUpStill”
    Created PickUpVert, which moves up and down on the y axis
    Added “PickupMoving” tag so moving pickups are worth more points
    Added line in PlayerControl to give double points for pickups with the “PickupMoving” tag
    Added PickUpHorz, which moves left and right on x axis, z axis, or diagonally on both
    </details>

---
+++ PickUpMoveVertical.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PickUpMoveVertical : MonoBehaviour
+{
+    // Determine how high it goes
+ 
+    public AnimationCurve myCurve;
+
+
+    //Use this for initialization
+    private void Start()
+    {
+        
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+{
+
+    transform.position = new Vector3(transform.position.x, myCurve.Evaluate((Time.time % myCurve.length)), transform.position.z);
+    }
+
+}
