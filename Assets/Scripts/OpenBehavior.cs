b781cc3f86195090148aed4886c3484c165f962d
IlyaPivavaruk <43891284+IlyaPivavaruk@users.noreply.github.com>
2018-10-12 10:42:37 -0700

    Prefabs/scripts

---
+++ Assets/Scripts/OpenBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBehavior : MonoBehaviour {
+    PlayerController userScript;
+    public int requiredScoreToDestroy;
+    void Start()
+    {
+        userScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+
+    }
+    void Update()
+    {
+        if (userScript.scoreCount >= requiredScoreToDestroy)
+        {
+            gameObject.SetActive(false);
+        }
+        
+    }
+}
