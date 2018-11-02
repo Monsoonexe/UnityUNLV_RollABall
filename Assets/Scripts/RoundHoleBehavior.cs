c07ddc33c42517bd9540b6612e75b0871019e543
NightlyShadow <43693128+NightlyShadow@users.noreply.github.com>
2018-11-01 23:43:44 -0700

    Added functionality and another wall
    Added script to the wall that the buddy can go through
    Added wall for player to go through and script for it
    Changed wall colors to correspond to  the object that can go through them (White for Player, Green for Buddy)
    Made a buddy prefab for its square version

---
+++ Assets/Scripts/RoundHoleBehavior.cs
@@ -0,0 +1,48 @@
+﻿/// Author: Name
+/// Date: Date
+/// Description: What program does
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundHoleBehavior : MonoBehaviour
+{
+    public GameManager gameManager;
+    public int scoreGiven = 10;
+    public float thrustPower = 20;
+    // Use this for initialization
+    void Start()
+    {
+        // if (gameManager == null) gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    /// <summary>
+    /// If a sphere goes through, force it out the way it came
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Buddy"))
+            other.GetComponent<Rigidbody>().AddForce(0, 0, -thrustPower, ForceMode.Impulse);
+    }
+
+
+    /// <summary>
+    /// If made all the way through, WIP: add to score and destroy obstacle
+    /// </summary>
+    private void OnTriggerExit(Collider other)
+    {
+        //gameManager.AddPoints(scoreGiven);
+        if(other.CompareTag("Player"))
+        {
+            //gameManager.AddPoints(scoreGiven);
+            Destroy(transform.parent.gameObject);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SquareHoleBehavior.cs
@@ -0,0 +1,47 @@
+﻿/// Author: Name
+/// Date: Date
+/// Description: What program does
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareHoleBehavior : MonoBehaviour
+{
+    public GameManager gameManager;
+    public int scoreGiven = 10;
+    public float thrustPower = 20;
+    // Use this for initialization
+    void Start()
+    {
+       // if (gameManager == null) gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    /// <summary>
+    /// If a sphere goes through, force it out the way it came
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            other.GetComponent<Rigidbody>().AddForce(0, 0, -thrustPower, ForceMode.Impulse);
+    }
+    
+
+    /// <summary>
+    /// If made all the way through, WIP: add to score and destroy obstacle
+    /// </summary>
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Buddy"))
+        {
+            //gameManager.AddPoints(scoreGiven);
+            Destroy(transform.parent.gameObject);
+        }
+            
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
